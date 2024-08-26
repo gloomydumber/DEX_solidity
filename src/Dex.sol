1671f2f3a6a272387b4a884e92f9724563d9cb15
gloomydumber <chromacrow@naver.com>
2024-08-26 22:05:20 +0900

    `refactor` : unnecessary import

--- src/Dex.sol
+++ src/Dex.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "forge-std/console.sol";
 
 contract Dex is ERC20 {
     address public tokenX;
